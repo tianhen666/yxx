9233c8a56fe6aa814897c4fa5c7c07b2ea21231f
tianhen <614891294@qq.com>
2023-07-05 15:10:54 +0800

    添加文章搜索功能

--- pages/main/news/news.vue
+++ pages/main/news/news.vue
@@ -1,83 +1,120 @@
 <template>
 	<!-- 提示登录组件 -->
 	<m-authorized-login ref="mLogin"></m-authorized-login>
-	
+
 	<!-- 加载提示 -->
 	<m-page-loading v-if="loading"></m-page-loading>
-	
+
 	<z-paging
 		ref="pagingObj"
 		v-model="mdataList"
 		@query="getListData"
 		created-reload
-		:loading-full-fixed="true"
 		show-loading-more-when-reload
-		min-delay="1000"
+		loading-full-fixed
 	>
-	
 		<!-- 固定顶部 -->
 		<template v-slot:top>
 			<!-- 背景 -->
-			<view class="pageBg"><image class="image" src="/static/images/bg.png" mode="aspectFill"></image></view>
+			<view class="pageBg">
+				<image class="image" src="/static/images/bg.png" mode="aspectFill"></image>
+			</view>
 			<!-- #ifndef H5 -->
 			<!-- 标题栏 -->
-			<uni-nav-bar statusBar fixed :title="'口腔知识'" color="#ffffff" :border="false"></uni-nav-bar>
+			<uni-nav-bar
+				statusBar
+				fixed
+				:title="'口腔知识'"
+				color="#ffffff"
+				:border="false"
+			></uni-nav-bar>
 			<view class="blank20"></view>
 			<!-- #endif -->
 		</template>
 
-		<view class="item" v-for="(item, index) in mdataList" :key="index">
-			<view
-				class="item-inner"
-				@tap="navigateTo(`/pages/sub1/newsDetails/newsDetails?targetId=${encodeURIComponent(item.id)}`)"
-			>
-				<view class="img_box"><image class="img" :src="item.thumbUrl" mode="aspectFill"></image></view>
-				<view class="right_box">
-					<view class="title">{{ item.title }}</view>
-					<view class="digest">{{ item.digest || item.title }}</view>
+		<view class="mybox123">
+			<!-- 搜索框 -->
+			<view class="search">
+				<uni-search-bar
+					v-model="searchVal"
+					@confirm="search"
+					placeholder="输入关键字搜索文章"
+					cancelButton="none"
+				></uni-search-bar>
+			</view>
+
+			<!-- 列表 -->
+			<view class="item" v-for="(item, index) in mdataList" :key="index">
+				<view
+					class="item-inner"
+					@tap="
+						navigateTo(
+							`/pages/sub1/newsDetails/newsDetails?targetId=${encodeURIComponent(
+								item.id
+							)}`
+						)
+					"
+				>
+					<view class="img_box">
+						<image class="img" :src="item.thumbUrl" mode="aspectFill"></image>
+					</view>
+					<view class="right_box">
+						<view class="title">{{ item.title }}</view>
+						<view class="digest">{{ item.digest || item.title }}</view>
+					</view>
 				</view>
 			</view>
 		</view>
-
-		<view></view>
 	</z-paging>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { onLoad, onShow } from '@dcloudio/uni-app'
-import { _freePublishGetFreePublish } from '@/aTemp/apis/wx.js'
-import { navigateTo } from '@/aTemp/utils/uniAppTools.js'
+import { ref } from 'vue';
+import { onLoad, onShow } from '@dcloudio/uni-app';
+import { _freePublishGetFreePublish } from '@/aTemp/apis/wx.js';
+import { navigateTo } from '@/aTemp/utils/uniAppTools.js';
 
 // z-ping对象
-const pagingObj = ref(null)
+const pagingObj = ref(null);
 
 // 加载中
-const loading = ref(true)
+const loading = ref(true);
 // 数据列表
-const mdataList = ref([])
-onLoad(options => {})
+const mdataList = ref([]);
+
+const searchVal = ref('');
+const search = val => {
+	pagingObj.value.reload();
+};
 
 // 获取文章列表
 const getListData = (pageNo, pageSize) => {
 	const params = {
 		pageNum: pageNo,
-		pageSize: pageSize
-	}
+		pageSize: pageSize,
+		searchVal: searchVal.value
+	};
 	_freePublishGetFreePublish(params)
 		.then(res => {
 			setTimeout(() => {
-				loading.value = false
-			}, 1000)
-			pagingObj.value.complete(res.data?.records || [])
+				loading.value = false;
+			}, 1000);
+			pagingObj.value.complete(res.data?.records || []);
 		})
 		.catch(error => {
-			pagingObj.value.complete(false)
-		})
-}
+			pagingObj.value.complete(false);
+		});
+};
 </script>
 
 <style lang="scss" scoped>
+.search {
+	margin: 0 32rpx;
+	:deep(.uni-searchbar) {
+		padding-left: 0;
+		padding-right: 0;
+	}
+}
 .item {
 	.item-inner {
 		@include mFlex;
